86a9ff34f3458842809f932b2af4b66c3d10a0dc
long2ge <444533452@qq.com>
2019-03-15 01:56:40 +0800

    add 添加OAuth2.0配置文件

---
+++ config/auth.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: LONG
+ * Date: 2019/3/6
+ * Time: 0:39
+ */
+return [
+    'defaults' => [
+        'guard' => 'api',
+        'passwords' => 'users',
+    ],
+    'guards' => [
+        'api' => [
+            'driver' => 'passport',
+            'provider' => 'users',
+        ],
+    ],
+    'providers' => [
+        'users' => [
+            'driver' => 'eloquent',
+            'model' => \App\User::class
+        ]
+    ]
+];
