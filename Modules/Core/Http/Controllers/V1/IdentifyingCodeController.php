9ea42f71091a4f2f4bda6fdeb0ed4e731f7df22e
long2ge <444533452@qq.com>
2019-04-07 00:04:46 +0800

    mod 修改代码格式

--- Modules/Core/Http/Controllers/V1/IdentifyingCodeController.php
+++ Modules/Core/Http/Controllers/V1/IdentifyingCodeController.php
@@ -36,11 +36,15 @@ class IdentifyingCodeController extends CoreController
         // 获取参数
         $requestArr = $request->only(array_keys($parameters));
 
-        // 校验手机验证码次数
-        $identifyingCodeService->identifyingCodeIsOverLimit($requestArr['phone_number']);
+        try {
 
-        // 保存验证码
-        $identifyingCodeService->saveIdentifyingCode($requestArr);
+            $identifyingCodeService->identifyingCodeIsOverLimit($requestArr['phone_number']); // 校验手机验证码次数
+
+            $identifyingCodeService->saveIdentifyingCode($requestArr); // 保存验证码
+
+        } catch (\Exception $e) {
+            $this->response()->errorBadRequest($e->getMessage());
+        }
 
         // 返回报文
         $this->response()->noContent();
