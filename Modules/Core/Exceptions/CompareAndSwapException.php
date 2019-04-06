3e3154de40c1dbd1e5bcef769e0971e9682be8bf
long2ge <444533452@qq.com>
2019-04-06 18:23:20 +0800

    add 增加异常类

---
+++ Modules/Core/Exceptions/CompareAndSwapException.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: long
+ * Date: 2019/3/23
+ * Time: 11:22 PM
+ */
+
+namespace Modules\Core\Exceptions;
+
+/**
+ * CAS异常类
+ * Class CompareAndSwapException
+ * @package Modules\Core\Exceptions
+ */
+class CompareAndSwapException extends \Exception
+{
+
+}
